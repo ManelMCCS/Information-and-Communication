0696a5e8d7ec2ddb98e78e61bcda9c92503ce9cf
ManelMCCS <manuelmagalhaes7@gmail.com>
2021-03-03 17:29:24 +0000

    Need to fix navbar and image

---
+++ js/html.js
@@ -0,0 +1,15 @@
+<script>
+
+    function headerHTLM(text, type)
+    {
+        info = '<header>
+            <div id = &#34;subject&#34;
+            >
+            <h2>About me... </h2>
+            </div>
+        </header>'
+        
+    }
+
+
+</script>
