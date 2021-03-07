0834dc49ae5f7a0c948bbaf08723e4885fb5b649
ManelMCCS <manuelmagalhaes7@gmail.com>
2021-03-07 13:47:31 +0000

    fix navbar and change style

--- js/html.js
+++ js/html.js
@@ -1,15 +1,5 @@
 <script>
 
-    function headerHTLM(text, type)
-    {
-        info = '<header>
-            <div id = &#34;subject&#34;
-            >
-            <h2>About me... </h2>
-            </div>
-        </header>'
-        
-    }
 
 
 </script>
